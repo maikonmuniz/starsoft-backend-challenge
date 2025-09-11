82e858622a348db31b14b104241dac9d38197911
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-09-11 10:24:34 -0300

    feat: create table order and item

---
+++ src/infra/database/order.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Item } from './item.entity';
+
+export enum OrderStatus {
+  PENDING = 'pendente',
+  PROCESSING = 'processando',
+  SENT = 'enviado',
+  DELIVERED = 'entregue',
+  CANCELED = 'cancelado',
+}
+
+@Entity()
+export class Order {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    description: string;
+
+    @OneToMany(() => Item, (item) => item.order, { cascade: true })
+    items: Item[];
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
+
+    @Column({
+        type: 'enum',
+        enum: OrderStatus,
+        default: OrderStatus.PENDING,
+    })
+    status: OrderStatus;
+}
