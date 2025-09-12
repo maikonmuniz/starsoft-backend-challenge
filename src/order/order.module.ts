30777fc39561dd20b70af63611cae2d5d50a98f0
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-09-12 15:44:15 -0300

    feat: add config the elasticSearch

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { Order } from '../infra/database/order.entity';
 import { OrderRepository } from '../infra/database/order.repository';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forFeature([Order]),
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        nodes: [configService.get<string>('ELASTIC_URL')],
+        auth: {
+          username: configService.get<string>('ELASTIC_USER') || 'elastic',
+          password: configService.get<string>('ELASTIC_PASS'),
+        },
+      }),
+    }),
     ClientsModule.register([
       {
         name: 'order_created',
